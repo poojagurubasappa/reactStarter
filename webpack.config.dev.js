6adb1bbd65b176ba1458c6cf87175db5c12bd9cf
Pooja Gurubasappa <Pooja.Gurubasappa@in.tesco.com>
2017-11-26 11:16:59 +0530

    weback configurations

---
+++ webpack.config.dev.js
@@ -0,0 +1,10 @@
+import webpack from 'webpack';
+import path from 'path';
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+}
