90dd4f1d509a01101ac2fa5d8baafb4ee44bcd70
Pooja Gurubasappa <Pooja.Gurubasappa@in.tesco.com>
2017-11-27 21:01:23 +0530

    use react router dom ti render the layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("HI");
+import 'babel-polyfill';
+ import React from 'react';
+ import { render } from 'react-dom';
+ import HomePage from './components/home/HomePage';
+ import ProductPage from './components/product/ProductPage';
+ import App from './components/App';
+ import { Route, BrowserRouter } from 'react-router-dom';
+ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+render(
+  <BrowserRouter>
+    <div>
+      <App>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/catalogue" component={ProductPage} />
+      </App>
+    </div>
+  </BrowserRouter>,
+  document.getElementById('app')
+);
